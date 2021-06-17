774b6ffa875a51b7a6f3973bd286d58bcca2ba7d
moaad99 <moadlzrak@gmail.com>
2021-06-17 23:20:33 +0100

    home page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Component/Navbar'
+import Review from './Component/Review'
+import Footer from './Component/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Review/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Navbar.css'
+export default function Navbar() {
+    return (  
+
+        <div  className="navbarr">
+          <nav  className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    
+    <a className="navbar-brand" href="#">   <img src="https://previews.123rf.com/images/scrap4vec/scrap4vec2005/scrap4vec200500184/148077898-logo-design-of-student-child-or-people-with-book-template-in-creative-shape-isolate-vector-illustrat.jpg" alt=""/> Student-Life</a>
+    
+    <div className="na" >
+      
+      
+       
+          <a style={{color:"black"}} className="nav-link" href="#"><strong>Sign-In</strong> </a>
+      
+
+        
+          <a style={{color:"black"}} className="nav-link" href="#">Sign-Up</a>
+    
+        
+     
+     
+     
+    </div>
+  </div>
+</nav>
+        </div>
+    )
+}
